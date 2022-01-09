a58cf65a8c5cdcf762dd24b777a622a9036db2fd
MEAN Hive <meanhive@zoho.com>
2022-01-09 16:38:34 +0530

    first commit

---
+++ config/index.js
@@ -0,0 +1,6 @@
+module.exports = {
+    prod: "mongodb+srv://novbep:test1234@cluster0.sswkn.mongodb.net/restapi?retryWrites=true&w=majority",
+    dev: "mongodb://localhost:27017/restapi",
+    accessToken: "afafsdjlj3asf4343lljlaf@#$agdasfa",
+    refreshToken: "afsfd;ljl;kjh4nlkhlafei%)(&,nlafh"
+};
